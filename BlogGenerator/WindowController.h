4e6172aed42ba9c5d026a43befa62cb8a8a36109
dj.yue <dj.yue@yuneec.com>
2017-09-19 14:22:32 +0800

    add insert image

---
+++ BlogGenerator/WindowController.h
@@ -0,0 +1,13 @@
+//
+//  WindowController.h
+//  BlogGenerator
+//
+//  Created by dj.yue on 2017/9/19.
+//  Copyright © 2017年 dj.yue. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WindowController : NSWindowController
+
+@end
---
+++ BlogGenerator/WindowController.m
@@ -0,0 +1,25 @@
+//
+//  WindowController.m
+//  BlogGenerator
+//
+//  Created by dj.yue on 2017/9/19.
+//  Copyright © 2017年 dj.yue. All rights reserved.
+//
+
+#import "WindowController.h"
+
+@interface WindowController ()
+
+@end
+
+@implementation WindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+- (IBAction)insertImageClick:(id)sender {
+}
+@end
